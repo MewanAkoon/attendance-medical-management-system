6bb899dbda387fb9f03ba0e81ee34304c13a0228
Damitha Amarakoon <damithamewan201@gmail.com>
2021-01-04 22:37:34 +0530

    medical search box addedd

--- src/components/medical/medicalRecords.jsx
+++ src/components/medical/medicalRecords.jsx
@@ -1,26 +1,82 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import moment from 'moment';
+
 import Loading from '../common/loading';
 import MedicalTable from './medicalTable';
 
+import { Breadcrumb } from 'react-bootstrap';
+
 class MedicalRecords extends Component {
-	state = { records: [], loading: true };
+	state = { records: [], filtered: [], loading: true };
 
 	async componentDidMount() {
 		const { data: records } = await axios.get(`/api/medicals`);
+		records.sort((a, b) => (moment(a).isSameOrAfter(b) ? 1 : -1));
 		this.setState({ records, loading: false });
 	}
 
-	componentWillUnmount() {}
+	renderBreadCrumbs() {
+		return (
+			<Breadcrumb>
+				<Breadcrumb.Item href='/home'>Home</Breadcrumb.Item>
+				<Breadcrumb.Item active>Medical Records</Breadcrumb.Item>
+			</Breadcrumb>
+		);
+	}
+
+	handleSearchBox = ({ currentTarget: { value: index } }) => {
+		const { records } = this.state;
+		let filtered = [];
+		if (index) {
+			console.log(index);
+			filtered = records.filter(record => record.index._id.includes(index));
+		}
+		this.setState({ filtered });
+	};
+
+	renderSearchBox() {
+		return (
+			<form className='form-inline justify-content-end d-flex md-form form-sm my-2'>
+				<i className='fa fa-search' aria-hidden='true' />
+				<input
+					className='form-control form-control-sm ml-2'
+					type='text'
+					name='search'
+					placeholder='Search Index'
+					aria-label='Search Index'
+					onChange={this.handleSearchBox}
+				/>
+			</form>
+		);
+	}
+
+	getRecords() {
+		const { records, filtered } = this.state;
+		return filtered.length > 0 ? filtered : records;
+	}
 
 	render() {
-		const { records, loading } = this.state;
+		const { loading } = this.state;
 
 		return (
 			<React.Fragment>
 				{!this.props.user.id && <Redirect to='/login' />}
-				{loading ? <Loading /> : <MedicalTable records={records} />}
+				{loading ? (
+					<Loading />
+				) : (
+					<React.Fragment>
+						{this.renderBreadCrumbs()}
+						<div>
+							<div className='jumbotron pt-4'>
+								<h1 className='text-center mb-2 dissplay-4'>Medical Records</h1>
+								{this.renderSearchBox()}
+								<MedicalTable records={this.getRecords()} />
+							</div>
+						</div>
+					</React.Fragment>
+				)}
 			</React.Fragment>
 		);
 	}
--- src/components/medical/medicalTable.jsx
+++ src/components/medical/medicalTable.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import moment from 'moment';
-import { Breadcrumb } from 'react-bootstrap';
 
 const noRecordsFound = () => (
 	<div className='alert alert-danger text-center w-50 mx-auto'>
@@ -12,47 +11,29 @@ const getDate = data => moment(data).format('MM-DD-YYYY');
 
 const getTime = data => moment(data).format('hh:mm:ss');
 
-const renderBreadCrumbs = () => {
-	return (
-		<Breadcrumb>
-			<Breadcrumb.Item href='/home'>Home</Breadcrumb.Item>
-			<Breadcrumb.Item active>Medical Records</Breadcrumb.Item>
-		</Breadcrumb>
-	);
-};
-
 const renderMedicalTable = records => (
-	<React.Fragment>
-		{renderBreadCrumbs()}
-		<div>
-			<div className='jumbotron pt-4'>
-				<h1 className='text-center mb-4 display-4'>Medical Records</h1>
-
-				<table className='table table-hover'>
-					<thead className='thead-dark'>
-						<tr>
-							<th>Index</th>
-							<th>Name</th>
-							<th>Reason</th>
-							<th>Date</th>
-							<th>Time</th>
-						</tr>
-					</thead>
-					<tbody>
-						{records.map(r => (
-							<tr key={r._id}>
-								<td>{r.index._id}</td>
-								<td>{r.index.username}</td>
-								<td>{r.reason}</td>
-								<td>{getDate(r.date)}</td>
-								<td>{getTime(r.date)}</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</React.Fragment>
+	<table className='table table-hover'>
+		<thead className='thead-dark'>
+			<tr>
+				<th>Index</th>
+				<th>Name</th>
+				<th>Reason</th>
+				<th>Date</th>
+				<th>Time</th>
+			</tr>
+		</thead>
+		<tbody>
+			{records.map(r => (
+				<tr key={r._id}>
+					<td>{r.index._id}</td>
+					<td>{r.index.username}</td>
+					<td>{r.reason}</td>
+					<td>{getDate(r.date)}</td>
+					<td>{getTime(r.date)}</td>
+				</tr>
+			))}
+		</tbody>
+	</table>
 );
 
 const MedicalTable = ({ records }) => {
